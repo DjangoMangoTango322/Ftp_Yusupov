5ff4b7c793b17d180f287b57c252a6b7bbecd109
student-a502 <student-a502@STUD-RM3-A502>
2025-11-21 15:29:31 +0500

    Реалезовал классы в Common и класс User в Server методы AutorizationUser и GetDirectory

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,40 @@
-﻿namespace Common
+﻿using System.Net;
+using Server;
+
+namespace Common
 {
     public class Programm
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAdress;
+        public static int Port;
+
+        public static bool AutorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+            return user != null;
+        }
+        public static List<string> GetDirectory(string src)
+        {
+            List<string> FoldersFiles = new List<string>();
+            if (Directory.Exists(src))
+            {
+                string[] dirs = Directory.GetDirectories(src);
+                foreach (string dir in dirs)
+                {
+                    string NameDirectory = dir.Replace(src, "");
+                    FoldersFiles.Add(NameDirectory + "/");
+                }
+                string[] files = Directory.GetFiles(src);
+                foreach (string file in files)
+                {
+                    string NameFile = file.Replace(src, "");
+                    FoldersFiles.Add(NameFile);
+                }
+            }
+            return FoldersFiles;
+        }
 
     }
-} 
+}
